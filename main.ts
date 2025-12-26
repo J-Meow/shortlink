27dad7ebd0b30f651e6b819d536a8529a2fe2e27
Jmeow <j@jmeow.net>
2025-12-26 16:04:57 -0600

    super basic test

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
+function getURLForId(id: string): string | null {
+    if (id == "testing") {
+        return "https://example.com"
+    }
+    return null
+}
+
 Deno.serve((req) => {
+    const url = new URL(req.url)
+    const id = url.pathname.slice(1).split("/")[0]
+    if (id) {
+        const redirectTo = getURLForId(id)
+        if (redirectTo) {
+            return new Response(null, {
+                status: 301,
+                headers: { location: redirectTo },
+            })
+        }
+    }
     return new Response(null, { status: 404 })
 })
